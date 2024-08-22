0a2cecdd3c1ea341fee4e51f9b3b11286e9a712d
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-08-22 17:05:48 -0400

    feat(auth:jwt): Add JTIMatcher strategy for JWT revocation
    * Add `jti` column to users table
    * Modify User model to include JTIMatcher strategy
    * Implement JWT token revocation using JTI
    * Refer to https://github.com/waiting-for-dev/devise-jwt?tab=readme-ov-file#jtimatcher for details

--- back_end/app/models/user.rb
+++ back_end/app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
+  # 
+  # Revocation strategies are a way to expire JWT tokens.
+  # We use the JTIMatcher strategy. It adds a new string column named `jti`
+  # to be added to the user. `jti` stands for JWT ID.
+  # Read more: https://github.com/waiting-for-dev/devise-jwt?tab=readme-ov-file#jtimatcher
+  devise :database_authenticatable, :registerable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 end
