4ca4d0cafe900353c119eb4eb033f117dd4d26e9
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-08-23 00:24:42 -0400

    controller(application): Handle ActiveRecord::RecordNotFound and ActiveRecord::RecordNotDestroyed exceptions
    * Add rescue_from blocks for specified exceptions
    * Define record_not_found and record_not_destroyed methods
    * Return standardized JSON error responses for these exceptions

--- back_end/app/controllers/application_controller.rb
+++ back_end/app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::API
+  include ResponseHandlerConcern
+
   respond_to :json
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :record_not_destroyed
+
+  def record_not_found
+    render json: error_response(message: "Record not found", errors: nil), status: :not_found
+  end
+
+  def record_not_destroyed
+    render json: error_response(message: "Record not destroyed", errors: nil), status: :unprocessable_entity
+  end
 
   def configure_devise_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
