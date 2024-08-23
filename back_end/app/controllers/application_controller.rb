8be127ddf3a01515c2043ba5418e69a4fa4f2f92
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-08-23 10:37:06 -0400

    controller(application)(auth): Handle CanCanCan AccessDenied exceptions
    * Add rescue_from block for CanCanCan::AccessDenied exceptions
    * Define access_denied method
    * Return standardized JSON error response for unauthorized access

--- back_end/app/controllers/application_controller.rb
+++ back_end/app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::API
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordNotDestroyed, with: :record_not_destroyed
+  rescue_from CanCan::AccessDenied, with: :access_denied
+
+  def access_denied
+    render json: error_response(message: "Access denied", errors: nil), status: :forbidden
+  end
 
   def record_not_found
     render json: error_response(message: "Record not found", errors: nil), status: :not_found
