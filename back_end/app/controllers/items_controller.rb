0c8466adad3bf7168ea505b28c1389f3b1de50e4
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-08-23 00:00:04 -0400

    controller(item): Generate ItemsController with CRUD actions
    * Use `rails g controller Items`
    * Include AuthenticationConcern and ResponseHandlerConcern
    * Implement basic CRUD logic for items within a task context
    * Return structured JSON responses for success and error cases

---
+++ back_end/app/controllers/items_controller.rb
@@ -0,0 +1,48 @@
+class ItemsController < ApplicationController
+  include AuthenticationConcern
+  include ResponseHandlerConcern
+  
+  before_action :set_task, only: [:create, :destroy, :update]
+  before_action :set_item, only: [:destroy, :update]
+
+  def create
+    item = @task.items.new(item_params)
+
+    if item.save
+      render json: success_response(message: "Resource created succesfully", data: { item: item }), status: :created
+    else
+      render json: error_response(message: "Failed to create resource", errors: item.errors ), status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    begin
+      @item.destroy!
+      render json: success_response(message: "Resource deleted successfully", data: nil), status: :ok
+    rescue StandardError => error
+      render json: error_response(message: "An unexpected error ocurred", errors: { base: error.message }), status: :internal_server_error
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: success_response(message: "Resource updated successfully", data: { item: @item }), status: :ok
+    else
+      render json: error_response(message: "Failed to update resource", errors: @item.errors), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :completed)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:task_id])
+  end
+
+  def set_item
+    @item = @task.items.find(params[:id])
+  end
+end
