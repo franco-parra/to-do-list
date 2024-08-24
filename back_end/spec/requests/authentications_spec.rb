08496f4916d434e0f7cff4ae1b53156a426b80c6
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-08-24 17:58:03 -0400

    refactor(test)(auth): Improve readability and organization of authentication tests * Refactor test structure for better understanding * Use more descriptive `describe` blocks and `context` blocks * Add comments to explain test cases

--- back_end/spec/requests/authentications_spec.rb
+++ back_end/spec/requests/authentications_spec.rb
@@ -2,38 +2,43 @@ require "rails_helper"
 require "devise/jwt/test_helpers"
 
 RSpec.describe "User authentication", type: :request do
-  let(:user_attributes) { attributes_for :user, :with_fixed_name, :with_fixed_email, :with_fixed_password }
-  let!(:user) { create(:user, user_attributes) }
-  let(:headers) { Devise::JWT::TestHelpers.auth_headers({}, user) }
-
-  describe "User registration" do
+  let(:user_attributes) { attributes_for :user }
+  
+  describe "POST /users" do
     it "registers a user successfully" do
-      post user_registration_path, params: { user: attributes_for(:user) }
+      post user_registration_path, params: { user: user_attributes }
       expect(response).to have_http_status(:success)
     end
   end
+  
+  context "when a user already exists" do
+    let!(:user) { create(:user, user_attributes) }
+    let(:headers) { Devise::JWT::TestHelpers.auth_headers({}, user) }
 
-  describe "User sign in" do
-    context "with correct credentials" do
-      it "signs in successfully" do
-        authenticate_user(user_attributes[:email], user_attributes[:password], :success)
+    describe "POST /users/sign_in" do
+      context "with correct credentials" do
+        it "signs in successfully" do
+          authenticate_user(user_attributes[:email], user_attributes[:password], :success)
+        end
+      end
+
+      context "with incorrect credentials" do
+        it "returns unauthorized status when credentials are incorrect" do
+          authenticate_user(user_attributes[:email], user_attributes[:password] + "@", :unauthorized)
+        end
       end
     end
 
-    context "with incorrect credentials" do
-      it "fails to sign in" do
-        authenticate_user(user_attributes[:email], user_attributes[:password] + "@", :unauthorized)
+    describe "DELETE /users/sign_out" do
+      context "when a user is already signed in" do
+        it "signs out the user succesfully" do
+          delete destroy_user_session_path, headers: headers
+          expect(response).to have_http_status(:success)
+    
+          get edit_user_registration_path, headers: headers
+          expect(response).to have_http_status(:unauthorized)
+        end
       end
-    end
-  end
-
-  describe "User sign out" do
-    it "signs out the user successfully" do
-      delete destroy_user_session_path, headers: headers
-      expect(response).to have_http_status(:success)
-
-      get edit_user_registration_path, headers: headers
-      expect(response).to have_http_status(:unauthorized)
     end
   end
 
