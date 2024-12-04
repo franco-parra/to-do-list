66edabdb60975eb1d8765a7713f28ae0d550ddae
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-12-04 20:24:29 -0300

    feat: add Spanish localization support
    Added internationalization to support Spanish in the API. Modified `application_controller.rb` to handle the `locale` parameter in each request, defaulting to English if not provided. The Spanish translations are included in the `es.yml` file.

--- back_end/app/controllers/application_controller.rb
+++ back_end/app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::API
   include ResponseHandlerConcern
 
   respond_to :json
+
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordNotDestroyed, with: :record_not_destroyed
   rescue_from CanCan::AccessDenied, with: :access_denied
@@ -22,5 +25,10 @@ class ApplicationController < ActionController::API
   def configure_devise_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 end
