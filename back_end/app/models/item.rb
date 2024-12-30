48afab367787365d8177e117ec48b80ca103147f
Franco Parra <franco.enrique.parra.campos@gmail.com>
2024-12-29 21:39:07 -0300

    fix: update validation for completed field in Item model
    The previous validation for the 'completed' field only allowed true as a valid value, treating false as empty. This change ensures that the 'completed' field can now accept both true and false, fixing the bug when saving tasks.

--- back_end/app/models/item.rb
+++ back_end/app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :task
-  validates :content, :completed, presence: true
+  validates :content, presence: true
+  validates :completed, inclusion: { in: [ true, false ] }
 end
